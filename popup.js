12c7ce4b4b24237774f64d0a220c4bb014a51983
Himanshunitrr <himanshumauryacrz@gmail.com>
2020-07-21 15:44:20 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  let button = document.getElementById("btn");
+  button.addEventListener("click", get_joke_of_the_day)
+  get_joke_of_the_day()
+}
+
+function get_joke_of_the_day() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Access the result here
+      let para = document.querySelector('p')
+      let jokeTitle = document.querySelector('h2')
+      let response = JSON.parse(this.responseText)
+      para.innerHTML = response.punchline
+      jokeTitle.innerHTML = response.setup
+    }
+  };
+  xhttp.open("GET", "https://official-joke-api.appspot.com/random_joke", true);
+  xhttp.send();
+}
+
